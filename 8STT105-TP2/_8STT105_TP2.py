fb82ad92a9199441fa4e8e6618bfe354e81dfe5d
Hexzhe <hexzhe@jsngbt.com>
2019-10-16 19:08:26 -0400

    Todos

--- 8STT105-TP2/_8STT105_TP2.py
+++ 8STT105-TP2/_8STT105_TP2.py
@@ -29,3 +29,16 @@ with open('eqarchive-en.csv') as csvfile: #first load
 
 print(f"Tremblements de terre chargés: {len(earthquakes)}")
 
+#TODO: Quelle est la probabilité d'avoir un tremblement de terre de magnitude 5 ou plus?
+
+#TODO: L'impact monétaire d'un tremblement de terre est fonction de sa magnétude
+#          0$ s'il est de moins de 1
+#          5k$ s'il est de 1 à moins de 2
+#          25k$ s'il est de 2 à moins de 3
+#          125k$ s'il est de 3 à moins de 4
+#          500k$ s'il est de 4 à moins de 5
+#          2000k$ s'il est de 5 à moins de 6
+#          10000k$ s'il est de 6 ou plus
+#      Quelle est l'espérance du coût des tremblements de terre au Canada sur 10 ans?
+
+#TODO: RAPPORT: Dans votre rapport, vous devez caractériser la population, l'échantillon, la v.a., la loi de probabilité, et justifiez vos réponses.
