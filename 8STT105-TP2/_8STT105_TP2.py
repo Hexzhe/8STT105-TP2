73429c5187b3a4e76dd08ded85460b3f661f659b
Hexzhe <hexzhe@jsngbt.com>
2019-10-16 19:06:08 -0400

    Basic load

--- 8STT105-TP2/_8STT105_TP2.py
+++ 8STT105-TP2/_8STT105_TP2.py
@@ -1,1 +1,31 @@
+import csv
+import statistics
+from datetime import datetime
+from dataclasses import dataclass
+
+@dataclass
+class Earthquake:
+    date: datetime
+    latitude: float
+    longitudes: float
+    depth: float
+    magnitude: float
+    magType: str
+    place: str
+
+print("8STT105 - TP2 - Partie 2")
+
+earthquakes = []
+
+with open('eqarchive-en.csv') as csvfile: #first load
+    readCSV = csv.reader(csvfile, delimiter=',')
+    first = True
+    for row in readCSV:
+        if first == True: #skip headers
+            first = False
+            continue
+
+        earthquakes.append(Earthquake(datetime.strptime(row[0], "%Y-%m-%dT%H:%M:%S%z"), float(row[1]), float(row[2]), float(row[3]), float(row[4]), row[5], row[6]))
+
+print(f"Tremblements de terre chargés: {len(earthquakes)}")
 
